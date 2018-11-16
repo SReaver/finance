d01c860fd53d67717cc46b781a1289ee15459668
Sergey <s.reaver@outlook.com>
2018-11-17 01:44:03 +0600

    исправлены ошибки

--- l1/main/script.js
+++ l1/main/script.js
@@ -1,21 +1,23 @@
 'use strict';
 let money = prompt("Ваш бюджет на месяц?");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
-let question1 = prompt("Введите обязательную статью расходов в этом месяце");
-let question2 = prompt("Во сколько обойдется?");
-let question3 = prompt("Введите обязательную статью расходов в этом месяце");
-let question4 = prompt("Во сколько обойдется?");
-let arrIncome = [];
+let answer1 = prompt("Введите обязательную статью расходов в этом месяце");
+let answer2 = prompt("Во сколько обойдется?");
+let answer3 = prompt("Введите обязательную статью расходов в этом месяце");
+let answer4 = prompt("Во сколько обойдется?");
+
 
 let appData = {
  budget: money,
  timeData: time,
- expenses: {
-  question1: question2,
-  question3: question4
- },
+ expenses: {},
  optionalExpenses: {},
- income: arrIncome,
+ income: [],
  savings: false
 };
+
+appData.expenses['Введите обязательную статью расходов в этом месяце'] = answer1;
+appData.expenses['Во сколько обойдется?'] = answer2;
+appData.expenses['Введите обязательную статью расходов в этом месяце'] = answer3;
+appData.expenses['Во сколько обойдется?'] = answer4;
 alert("Ваш бюджет на 1 день: " + money / 30);

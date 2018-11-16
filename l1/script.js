c32b18b8da8c14bb97be78727ebdc3a5ac94cfe5
Sergey <s.reaver@outlook.com>
2018-11-16 17:46:08 +0600

    Это наш первый комит

---
+++ l1/script.js
@@ -0,0 +1,20 @@
+'use strict';
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let question1 = prompt("Введите обязательную статью расходов в этом месяце");
+let question2 = prompt("Во сколько обойдется?");
+let question3 = prompt("Введите обязательную статью расходов в этом месяце");
+let question4 = prompt("Во сколько обойдется?");
+let arrIncome = [];
+let appData = {
+ budget: money,
+ timeData: time,
+ expenses: {
+  question1: question2,
+  question3: question4
+ },
+ optionalExpenses: {},
+ income: arrIncome,
+ savings: false
+};
+alert("Ваш бюджет на 1 день: " + money / 30);

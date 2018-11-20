5c4cf4dcb728d4a57979624ec0b01776ab839fa4
Sergey <s.reaver@outlook.com>
2018-11-20 18:52:04 +0600

    1-3

--- l3/additional/script.js
+++ l3/additional/script.js
@@ -1,8 +1,12 @@
 'use strict'
 let str = "урок - 3 - был слишком легким";
 
-str = str[0].toLocaleUpperCase() + str.slice(1);
-//alert(str);
-str.replace(\-\g, " ");
+str = str[0].toUpperCase() + str.slice(1);
+alert(str);
+str = str.replace(/-/g, ' ');
 alert(str);
 console.log(str);
+str = str.slice(str.indexOf('легким'));
+console.log(str);
+str = str.replace(/[и, м]/g, "о");
+alert(str);

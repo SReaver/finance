12a356c786d7fbbd3bc275c2f79e796656a99a80
Sergey <s.reaver@outlook.com>
2018-11-22 23:35:46 +0600

    Fixed L5 Main 1

--- l5/additional/script.js
+++ l5/additional/script.js
@@ -1,26 +1,11 @@
 'use strict'
-let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
-let today = new Date();
-//alert(today.getDay());
-for (let i = 0; i < week.length; i++) {
- if (i == week.length - 1 || i == week.length - 2) {
-  if (i == today.getDay() - 1 || i == today.getDay() + 6) {
-   document.write(week[i].italics().bold() + '<br>');
-  } else {
-   document.write(week[i].bold() + '<br>');
-  }
- } else {
-  if (i == today.getDay() - 1) {
-   document.write(week[i].italics() + '<br>');
-  } else {
-   document.write(week[i] + '<br>');
-  }
- }
-}
-
-let arr = ["346125413", "230948578", "9827452293847", "723489320480932", "983274982374823", "30923745928735", "93249823409348"];
-for (let a = 0; a < arr.length; a++) {
- if (arr[a].startsWith("3") || arr[a].startsWith("7")) {
-  console.log(arr[a]);
- }
-}
+//1) Выведите на страницу текущую дату и время в формате '09:59:59 30.05.2018'
+let date = new Date();
+let hour = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
+let month = date.getMonth();
+let year = date.getFullYear();
+let weekDay = date.getDay();
+let days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+console.log(day);
--- l5/main/1/js/script.js
+++ l5/main/1/js/script.js
@@ -1,17 +1,17 @@
-let menu = document.querySelectorAll(".menu");
+let menu = document.querySelector(".menu");
 let liItem = document.querySelectorAll("li");
-let liItem2 = liItem[2];
-menu[0].insertBefore(liItem2, liItem[1]);
+
+menu.insertBefore(liItem[2], liItem[1]);
 
 let li5 = document.createElement('li');
 li5.classList.add('menu-item');
 li5.innerHTML = 'Пятый пункт';
-menu[0].appendChild(li5);
+menu.appendChild(li5);
 
 document.body.style.backgroundImage = "url('img/apple_true.jpg')";
 
-let title = document.querySelectorAll("#title");
-title[0].innerHTML = "Мы продаем только подлинную технику Apple";
+let title = document.querySelector("#title");
+title.innerHTML = "Мы продаем только подлинную технику Apple";
 
 let column = document.querySelectorAll(".column");
 let adv = document.getElementsByClassName("adv");

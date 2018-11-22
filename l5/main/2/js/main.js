d7915dec3c5a096b1f4ae8e96d548f65ae12b2d5
Sergey <s.reaver@outlook.com>
2018-11-22 21:48:40 +0600

    L5 Main 2 Done

---
+++ l5/main/2/js/main.js
@@ -0,0 +1,28 @@
+let startButton = document.getElementById("start");
+
+let budgetValue = document.getElementsByClassName("budget-value");
+let dayBudgetValue = document.getElementsByClassName("daybudget-value");
+let levelValue = document.getElementsByClassName("level-value");
+let expensesValue = document.getElementsByClassName("expenses-value");
+let optionalExpensesValue = document.getElementsByClassName("optionalexpenses-value");
+let incomeValue = document.getElementsByClassName("income-value");
+let monthSavingsValue = document.getElementsByClassName("monthsavings-value");
+let yearSavingsValue = document.getElementsByClassName("yearsavings-value");
+let yearValue = document.getElementsByClassName("year-value");
+let monthValue = document.getElementsByClassName("month-value");
+let dayValue = document.getElementsByClassName("day-value");
+
+let expensesInput = document.getElementsByClassName("expenses-item");
+
+let submitOptionalExpensesBtn = document.getElementsByTagName("optionalexpenses-btn");
+let countBudgetBtn = document.getElementsByTagName("count-budget-btn");
+
+let optionalExpensesInput = document.querySelectorAll(".optionalexpenses-item");
+
+let chooseIncomeText = document.querySelector(".choose-income");
+let chbSavings = document.querySelector(".savings");
+let chooseSumText = document.querySelector(".choose-sum");
+let choosePercentText = document.querySelector(".choose-percent");
+let yearInput = document.querySelector(".year-value");
+let monthInput = document.querySelector(".month-value");
+let dayInput = document.querySelector(".day-value");
